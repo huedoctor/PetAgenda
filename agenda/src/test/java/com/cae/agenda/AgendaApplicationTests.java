49b427004fee13e7bd5a0fe4faba92d64f38a290
huedoctor <huedoctor35@gmail.com>
2023-11-12 16:54:18 -0300

    base springboot

---
+++ agenda/src/test/java/com/cae/agenda/AgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cae.agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
